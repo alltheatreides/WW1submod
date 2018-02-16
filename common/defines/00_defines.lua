f96cce387c8e1d04e11e9441f809ff5f6e0b9692
Atreides <goldschmidt.antoine@gmail.com>
2018-02-16 15:57:29 +0100

    ww1_7
    Artillery Shells!

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -338,7 +338,7 @@ NMilitary = {
 
 	RECON_SKILL_IMPACT = 10, -- how many skillpoints is a recon advantage worth when picking a tactic.
 
-	MAX_DIVISION_BRIGADE_WIDTH = 4,			-- Max width of regiments in division designer.
+	MAX_DIVISION_BRIGADE_WIDTH = 5,			-- Max width of regiments in division designer.
 	MAX_DIVISION_BRIGADE_HEIGHT = 5,		-- Max height of regiments in division designer.
 	MAX_DIVISION_SUPPORT_WIDTH = 2,			-- Max width of support in division designer.
 	MAX_DIVISION_SUPPORT_HEIGHT = 5,		-- Max height of support in division designer.
